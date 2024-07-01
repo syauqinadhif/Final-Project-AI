e41f21cfe33d24eb35658bc2a1b37c585c941553
syauqinadhif <syauqinn71@gmail.com>
2024-07-01 20:08:25 +0700

    delete covid function

--- main.py
+++ main.py
@@ -8,80 +8,6 @@ from geopy.geocoders import Nominatim
 from geopy.distance import great_circle
 import wikipedia
 
-class COVID:
-    def __init__(self):
-        self.total = 'Not Available'
-        self.deaths = 'Not Available'
-        self.recovered = 'Not Available'
-        self.totalIndia = 'Not Available'
-        self.deathsIndia = 'Not Available'
-        self.recoveredIndia = 'Not Available'
-
-    def covidUpdate(self):
-        URL = 'https://www.worldometers.info/coronavirus/'
-        result = requests.get(URL)
-        src = result.content
-        soup = BeautifulSoup(src, 'html.parser')
-
-        temp = []
-        divs = soup.find_all('div', class_='maincounter-number')
-        for div in divs:
-            temp.append(div.text.strip())
-        self.total, self.deaths, self.recovered = temp[0], temp[1], temp[2]
-
-    def covidUpdateIndia(self):
-        URL = 'https://www.worldometers.info/coronavirus/country/india/'
-        result = requests.get(URL)
-        src = result.content
-        soup = BeautifulSoup(src, 'html.parser')
-
-        temp = []
-        divs = soup.find_all('div', class_='maincounter-number')
-        for div in divs:
-            temp.append(div.text.strip())
-        self.totalIndia, self.deathsIndia, self.recoveredIndia = temp[0], temp[1], temp[2]
-
-    def totalCases(self, india_bool):
-        if india_bool:
-            return self.totalIndia
-        return self.total
-
-    def totalDeaths(self, india_bool):
-        if india_bool:
-            return self.deathsIndia
-        return self.deaths
-
-    def totalRecovery(self, india_bool):
-        if india_bool:
-            return self.recoveredIndia
-        return self.recovered
-
-    def symptoms(self):
-        symt = ['1. Fever',
-                '2. Coughing',
-                '3. Shortness of breath',
-                '4. Trouble breathing',
-                '5. Fatigue',
-                '6. Chills, sometimes with shaking',
-                '7. Body aches',
-                '8. Headache',
-                '9. Sore throat',
-                '10. Loss of smell or taste',
-                '11. Nausea',
-                '12. Diarrhea']
-        return symt
-
-    def prevention(self):
-        prevention = ['1. Clean your hands often. Use soap and water, or an alcohol-based hand rub.',
-                      '2. Maintain a safe distance from anyone who is coughing or sneezing.',
-                      '3. Wear a mask when physical distancing is not possible.',
-                      '4. Don’t touch your eyes, nose or mouth.',
-                      '5. Cover your nose and mouth with your bent elbow or a tissue when you cough or sneeze.',
-                      '6. Stay home if you feel unwell.',
-                      '7. If you have a fever, cough and difficulty breathing, seek medical attention.']
-        return prevention
-
-
 class WEATHER:
     def __init__(self):
         self.tempValue = ''
@@ -126,46 +52,15 @@ class WEATHER:
         return [self.tempValue, self.currCondition, today, self.city, self.speakResult]
 
 
-c = COVID()
 w = WEATHER()
 
 
 def dataUpdate():
-    c.covidUpdate()
-    c.covidUpdateIndia()
     w.updateWeather()
 
 
 def weather():
     return w.weather()
-
-
-def covid(query):
-    if "india" in query:
-        india_bool = True
-    else:
-        india_bool = False
-
-    if "statistic" in query or 'report' in query:
-        return ["Here are the statistics...", ["Total cases: " + c.totalCases(india_bool),
-                                              "Total Recovery: " + c.totalRecovery(india_bool),
-                                              "Total Deaths: " + c.totalDeaths(india_bool)]]
-
-    elif "symptom" in query:
-        return ["Here are the Symptoms...", c.symptoms()]
-
-    elif "prevent" in query or "measure" in query or "precaution" in query:
-        return ["Here are the some of preventions from COVID-19:", c.prevention()]
-
-    elif "recov" in query:
-        return "Total Recovery is: " + c.totalRecovery(india_bool)
-
-    elif "death" in query:
-        return "Total Deaths are: " + c.totalDeaths(india_bool)
-
-    else:
-        return "Total Cases are: " + c.totalCases(india_bool)
-
 
 def latestNews(news=5):
     URL = 'https://www.cnnindonesia.com/nasional'
@@ -324,15 +219,6 @@ def main():
 
         if user_input in ['quit', 'exit', 'bye']:
             break
-
-        elif 'covid' in user_input or 'corona' in user_input:
-            dataUpdate()
-            response = covid(user_input)
-            if isinstance(response, list):
-                for item in response:
-                    print(item)
-            else:
-                print(response)
 
         elif 'weather' in user_input or 'temperature' in user_input or 'forecast' in user_input:
             w.updateWeather()
