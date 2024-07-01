5f3f648d3ec6a9e04f41d6472d5c307d1045a8fc
antoniusbayu76 <antoniusbayu96@gmail.com>
2024-07-01 20:53:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class WEATHER:
                 pass
 
         divs = soup.find_all(
-            'div', class_='CurrentConditions--phraseValue--2xXSr')
+            'div', class_='CurrentConditions--phraseValue--mZC_p')
         for div in divs:
             self.currCondition = div.text
             break
